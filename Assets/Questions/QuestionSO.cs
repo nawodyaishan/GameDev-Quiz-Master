12945157cca3cba9fc9e46de86cb4162c16dd201
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-06-07 22:18:28 +0530

    Add fields and properties to serialised field

--- Assets/Questions/QuestionSO.cs
+++ Assets/Questions/QuestionSO.cs
@@ -2,9 +2,21 @@ using UnityEngine;
 
 namespace Questions
 {
-    [CreateAssetMenu(menuName = "QuizQuestion", fileName = "New Question")]
+    [CreateAssetMenu(menuName = "QuizQuestion", fileName = "Question")]
     public class QuestionSO : ScriptableObject
     {
-        string question = "Enter New Question Text Here";
+        // for the question
+        [TextArea(2, 6)] [SerializeField] string question = "Enter New Question Text Here";
+
+        // for the answers
+        [SerializeField] string[] answers = new string[4];
+
+        // For the o=correct answer
+        [SerializeField] int correctAnswer;
+
+        public string GetQuestion()
+        {
+            return question;
+        }
     } // Class
 }
