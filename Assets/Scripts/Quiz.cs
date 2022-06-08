d9efb2cb3f2cc554caf57ed4f2bd67d6654e849f
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-06-08 12:09:31 +0530

    Add answer text buttons data to UI

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,17 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using Questions;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] QuestionSO question;
     [SerializeField] TextMeshProUGUI questionText;
-    [SerializeField] private GameObject[] answerButtons; 
+    [SerializeField] private GameObject[] answerButtons;
+
+    private int correctAnswerIndex;
+
+    [SerializeField] private Sprite defaultAnswerSprite;
+    [SerializeField] private Sprite correctAnswerSprite;
 
     void Start()
     {
         questionText.text = question.GetQuestion();
+        AnswersToButton();
+    }
+
+    void AnswersToButton()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct";
+            Image buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
     }
 }
